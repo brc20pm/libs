6ae94e77e3f9db51dd1476feb2a35945f0bfd5d3
Fapiao-BTC <156578846+Fapiao-BTC@users.noreply.github.com>
2024-02-17 03:56:31 +0800

    312321

--- libs/evm.js
+++ libs/evm.js
@@ -1,17 +1,19 @@
-//使用EVMCaller向外暴露接口
-
-let _etherJs = ethers;
-
-var EVMCaller = {
-	call: function(msg) {
-		try {
-			const interface = new _etherJs.utils.Interface(msg.abi);
-			const data = interface.encodeFunctionData(msg.method, msg.args);
-			const encodedData = EVM_CALL(msg.rpcUrl, msg.contract, data);
-			const decodedData = interface.decodeFunctionResult(msg.method, encodedData);
-			return decodedData.toString()
-		} catch (e) {
-			throw new Error(e);
+{
+	//使用EVMCaller向外暴露接口
+	
+	let _etherJs = ethers;
+	
+	var EVMCaller = {
+		call: function(msg) {
+			try {
+				const interface = new _etherJs.utils.Interface(msg.abi);
+				const data = interface.encodeFunctionData(msg.method, msg.args);
+				const encodedData = EVM_CALL(msg.rpcUrl, msg.contract, data);
+				const decodedData = interface.decodeFunctionResult(msg.method, encodedData);
+				return decodedData.toString()
+			} catch (e) {
+				throw new Error(e);
+			}
 		}
 	}
 }
