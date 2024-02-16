3d05995be83157ba152119d41564e7ca0bb1ec59
Fapiao-BTC <156578846+Fapiao-BTC@users.noreply.github.com>
2024-02-16 23:02:25 +0800

    2313

--- libs/evm.js
+++ libs/evm.js
@@ -4,6 +4,7 @@
 function EVMCaller(rpcUrl, abi, contract, method, args = []) {
 	const interface = new ethers.utils.Interface(abi);
 	const data = interface.encodeFunctionData(method, args);
+	throw new Error(data)
 	//调用合约
 	const encodedData = EVM_CALL(rpcUrl,contract,data);
 	const decodedData = interface.decodeFunctionResult(method, encodedData);
